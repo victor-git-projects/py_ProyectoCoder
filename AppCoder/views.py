ce17bd6a47d07a526ee85ca80a59e59f8272b476
Victor Davila <victordavila04@gmail.com>
2023-02-09 20:48:28 -0500

    Se realizo otra modificacion

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,12 +3,25 @@ from django.http import HttpResponse
 from AppCoder.models import Curso
 
 # Create your views here.
+def inicio(request):
+    return HttpResponse("vista inicio")
 
-def curso(self):
-    curso = Curso(nombre="Desarrollo web", comision = 19881)
-    curso.save()
-    documentoDeTexto = f"--> Curso: {curso.nombre} comision: {curso.comision}"
-    return HttpResponse(documentoDeTexto)
+def cursos(request):
+    return HttpResponse("vista curso")
+
+def profesores(request):
+    return HttpResponse("vista profesores")    
+
+def entregables(request):
+    return HttpResponse("vista entregables")
+
+def estudiantes(request):
+    return HttpResponse("vista estudiantes")
+    
+ #   curso = Curso(nombre="Desarrollo web", comision = 19881)
+ #   curso.save()
+ #   documentoDeTexto = f"--> Curso: {curso.nombre} comision: {curso.comision}"
+ #   return HttpResponse(documentoDeTexto)
 
 
 
